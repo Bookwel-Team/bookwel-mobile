384ec240b4fcc1b9d2e3595146125c5418eeaca2
Tsirimaholy <hei.tsirimaholy@gmail.com>
2024-02-03 18:18:48 +0300

    refactor: using actionfail actionSucces pattern

--- app/models/AuthenticationStore.ts
+++ app/models/AuthenticationStore.ts
@@ -10,39 +10,57 @@ export const AuthenticationStoreModel = types
     uid: "",
     user: types.maybeNull(UserModel),
   })
-  .actions((store) => ({
-    login: flow(function* (email:string, password: string) {
-      try {
-        const userCredential: FirebaseAuthTypes.UserCredential = yield auth().signInWithEmailAndPassword(email, password);
+  .actions((store) => {
+    const loginSuccess = flow(function* (user: FirebaseAuthTypes.User) {
+      store.authEmail = user.email
+      store.uid = user.uid
+      store.authToken = yield user.getIdToken()
+    })
 
-        const user = userCredential.user
-        store.authEmail = user.email;
-        store.uid = user.uid
-        store.authToken = yield user.getIdToken();
-        __DEV__ && console.tron.log("user logged in!")
-      }catch (e) {
-        if (e.code === 'auth/email-already-in-use') {
-          __DEV__ && console.tron.log("That email address is already in use!")
-        }else if (e.code==='auth/invalid-credential'){
-          __DEV__ && console.error("The supplied auth credential is incorrect, malformed or has expired.");
-        }
-        __DEV__ && console.error(e);
-        __DEV__ && console.tron.error(e.message, e);
-        throw e;
+    const loginFail = (e) => {
+      if (e.code === "auth/email-already-in-use") {
+        __DEV__ && console.tron.log("That email address is already in use!")
+      } else if (e.code === "auth/invalid-credential") {
+        __DEV__ && console.error("The supplied auth credential is incorrect, malformed or has expired.")
+      } else {
+        __DEV__ && console.tron.error(e.message, e)
       }
     }
-    ),
-    logout: flow(function*() {
+
+    const login = flow(function* (email: string, password: string) {
       try {
-        yield auth().signOut();
-        store.authToken = undefined
-        store.authEmail = ""
-      }catch (e) {
-        __DEV__ && console.tron.error("fail to logout", e);
-        throw e;
+        const userCredential: FirebaseAuthTypes.UserCredential = yield auth().signInWithEmailAndPassword(email, password)
+        const user = userCredential.user
+        yield loginSuccess(user)
+        __DEV__ && console.tron.log("user logged in!")
+      } catch (e) {
+        loginFail(e)
+        throw e
       }
-    }),
-  }))
+    })
+
+    return { login }
+  }).actions(store => {
+    const logoutSuccess = () => {
+      store.authToken = undefined
+      store.authEmail = ""
+    }
+    const logoutFail = (e) => {
+      __DEV__ && console.tron.error("fail to logout", e)
+    }
+
+    const logout = flow(function* () {
+      try {
+        yield auth().signOut()
+        logoutSuccess()
+      } catch (e) {
+        logoutFail(e)
+        throw e
+      }
+    })
+
+    return { logout }
+  })
   .views((store) => ({
     get isAuthenticated() {
       return !!store.authToken
@@ -64,5 +82,8 @@ export const AuthenticationStoreModel = types
     },
   }))
 
-export interface AuthenticationStore extends Instance<typeof AuthenticationStoreModel> {}
-export interface AuthenticationStoreSnapshot extends SnapshotOut<typeof AuthenticationStoreModel> {}
+export interface AuthenticationStore extends Instance<typeof AuthenticationStoreModel> {
+}
+
+export interface AuthenticationStoreSnapshot extends SnapshotOut<typeof AuthenticationStoreModel> {
+}
