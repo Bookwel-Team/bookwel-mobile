3331e5be8ad4a40ef7408214b36f99bcacaffdd9
Tsirimaholy <hei.tsirimaholy@gmail.com>
2024-02-09 15:44:12 +0300

    feat: login problems

--- app/services/api/auth-api.ts
+++ app/services/api/auth-api.ts
@@ -21,5 +21,8 @@ export const AUTH_PROBLEMS = {
   authEmailAlreadyInUse: "auth/email-already-in-use",
   emailInvalid: "auth/invalid-email",
   weakPassword: "auth/weak-password",
+  wrongPassword: "auth/wrong-password",
   authInvalidCredential: "auth/invalid-credential",
+  userNotFund: "auth/user-not-found",
+  userDisabled: "auth/user-disabled",
 } as const
